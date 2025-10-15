5cdef40bdc2cd9f986e5e120dd61371f743c2b72
filippbrokhman <filipp1303@live.com>
2025-10-15 11:52:48 -0400

    created a mockup of the about us section.

--- webmockupv/src/components/AboutUs.js
+++ webmockupv/src/components/AboutUs.js
@@ -2,7 +2,77 @@ import React from "react";
 
 function AboutUs() {
     return (
-        <h1>About Us Test</h1>
+        <div>   
+            <section className="body-text-white">
+                <div className="body-content-container">
+                    <ul>
+                        <h2> About Us</h2>
+                        <p>Lorem ipsum dolor sit amet, 
+                            consectetur adipiscing elit. 
+                            Vestibulum sollicitudin, metus sit amet vehicula commodo, 
+                            lectus lectus vehicula purus, nec dapibus risus dolor at nisi. 
+                            Suspendisse et iaculis magna, sed malesuada est. 
+                            Nunc vestibulum laoreet tempus. 
+                            Fusce tristique, velit et congue viverra, 
+                            leo libero tristique nisl, nec interdum eros odio eu ante. 
+                            Nunc egestas elementum ipsum, in fermentum diam eleifend id. 
+                            Duis sit amet faucibus tortor, sed auctor ipsum. 
+                            Sed auctor facilisis felis at suscipit. Vestibulum congue dignissim
+                            placerat. Fusce posuere elit in bibendum ultricies. 
+                            Maecenas eget tellus a diam accumsan maximus ut sit amet turpis. 
+                            Orci varius natoque penatibus et magnis dis parturient montes, 
+                            nascetur ridiculus mus. Donec ut ornare odio. Suspendisse et laoreet
+                            mi.</p>
+                    </ul>
+                </div>
+            </section>
+            <section className="body-text-blue">
+                <div className="body-content-container">
+                    <ul>
+                        <h2> Our Mission and Values</h2>
+                        <p>Lorem ipsum dolor sit amet, 
+                            consectetur adipiscing elit. 
+                            Vestibulum sollicitudin, metus sit amet vehicula commodo, 
+                            lectus lectus vehicula purus, nec dapibus risus dolor at nisi. 
+                            Suspendisse et iaculis magna, sed malesuada est. 
+                            Nunc vestibulum laoreet tempus. 
+                            Fusce tristique, velit et congue viverra, 
+                            leo libero tristique nisl, nec interdum eros odio eu ante. 
+                            Nunc egestas elementum ipsum, in fermentum diam eleifend id. 
+                            Duis sit amet faucibus tortor, sed auctor ipsum. 
+                            Sed auctor facilisis felis at suscipit. Vestibulum congue dignissim
+                            placerat. Fusce posuere elit in bibendum ultricies. 
+                            Maecenas eget tellus a diam accumsan maximus ut sit amet turpis. 
+                            Orci varius natoque penatibus et magnis dis parturient montes, 
+                            nascetur ridiculus mus. Donec ut ornare odio. Suspendisse et laoreet
+                            mi.</p>
+                    </ul>
+                </div>
+            </section>
+            <section className="body-text-white">
+                <div className="body-content-container">
+                    <ul>
+                        <h2> About Us</h2>
+                        <p>Lorem ipsum dolor sit amet, 
+                            consectetur adipiscing elit. 
+                            Vestibulum sollicitudin, metus sit amet vehicula commodo, 
+                            lectus lectus vehicula purus, nec dapibus risus dolor at nisi. 
+                            Suspendisse et iaculis magna, sed malesuada est. 
+                            Nunc vestibulum laoreet tempus. 
+                            Fusce tristique, velit et congue viverra, 
+                            leo libero tristique nisl, nec interdum eros odio eu ante. 
+                            Nunc egestas elementum ipsum, in fermentum diam eleifend id. 
+                            Duis sit amet faucibus tortor, sed auctor ipsum. 
+                            Sed auctor facilisis felis at suscipit. Vestibulum congue dignissim
+                            placerat. Fusce posuere elit in bibendum ultricies. 
+                            Maecenas eget tellus a diam accumsan maximus ut sit amet turpis. 
+                            Orci varius natoque penatibus et magnis dis parturient montes, 
+                            nascetur ridiculus mus. Donec ut ornare odio. Suspendisse et laoreet
+                            mi.</p>
+                    </ul>
+                </div>
+            </section>
+        </div> 
     )
 }
 
