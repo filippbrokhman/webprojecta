a3134a1121c954328a261ca4e168413024eefaa3
filippbrokhman <filipp1303@live.com>
2025-10-15 11:36:31 -0400

    template mockup of the home section. complete with template images and resizing functionality, alongside background colours. Also fixed a bug.

--- webmockupv/src/components/Home.js
+++ webmockupv/src/components/Home.js
@@ -1,8 +1,91 @@
 import React from "react";
+import './component.css'
 
-function Home() {
+const Home = () => {
     return (
-        <h1>Home Page Test</h1>
+        <div>
+            <section className="body-variant-white">
+                <div className="Media">
+                    <img src={require("../Images/kitchen-casement-arched.jpeg")} alt="Arched window"></img>
+                </div>
+                <div className="body-content">
+                    <ul>
+                        <h1> content header test</h1>
+                        <p>Lorem ipsum dolor sit amet, 
+                            consectetur adipiscing elit. 
+                            Vestibulum sollicitudin, metus sit amet vehicula commodo, 
+                            lectus lectus vehicula purus, nec dapibus risus dolor at nisi. 
+                            Suspendisse et iaculis magna, sed malesuada est. 
+                            Nunc vestibulum laoreet tempus. 
+                            Fusce tristique, velit et congue viverra, 
+                            leo libero tristique nisl, nec interdum eros odio eu ante. 
+                            Nunc egestas elementum ipsum, in fermentum diam eleifend id. 
+                            Duis sit amet faucibus tortor, sed auctor ipsum. 
+                            Sed auctor facilisis felis at suscipit. Vestibulum congue dignissim
+                            placerat. Fusce posuere elit in bibendum ultricies. 
+                            Maecenas eget tellus a diam accumsan maximus ut sit amet turpis. 
+                            Orci varius natoque penatibus et magnis dis parturient montes, 
+                            nascetur ridiculus mus. Donec ut ornare odio. Suspendisse et laoreet
+                            mi.</p>
+                        <p>Will be a button with link soon</p>
+                    </ul>
+                </div>
+            </section>
+            <section className="body-variant-blue">
+                <div className="body-content">
+                    <ul>
+                        <h1> content header test</h1>
+                        <p>Lorem ipsum dolor sit amet, 
+                            consectetur adipiscing elit. 
+                            Vestibulum sollicitudin, metus sit amet vehicula commodo, 
+                            lectus lectus vehicula purus, nec dapibus risus dolor at nisi. 
+                            Suspendisse et iaculis magna, sed malesuada est. 
+                            Nunc vestibulum laoreet tempus. 
+                            Fusce tristique, velit et congue viverra, 
+                            leo libero tristique nisl, nec interdum eros odio eu ante. 
+                            Nunc egestas elementum ipsum, in fermentum diam eleifend id. 
+                            Duis sit amet faucibus tortor, sed auctor ipsum. 
+                            Sed auctor facilisis felis at suscipit. Vestibulum congue dignissim
+                            placerat. Fusce posuere elit in bibendum ultricies. 
+                            Maecenas eget tellus a diam accumsan maximus ut sit amet turpis. 
+                            Orci varius natoque penatibus et magnis dis parturient montes, 
+                            nascetur ridiculus mus. Donec ut ornare odio. Suspendisse et laoreet
+                            mi.</p>
+                        <p>Will be a button with link soon</p>
+                    </ul>
+                </div>
+                <div className="Media">
+                    <img src={require("../Images/multi-windows.jpeg")} alt="multiple windows in a row"></img>
+                </div>
+            </section>
+            <section className="body-variant-white">
+                <div className="Media">
+                    <img src={require("../Images/Sliding Windows-min.jpg")} alt="kitchen window"></img>
+                </div>
+                <div className="body-content">
+                    <ul>
+                        <h1> content header test</h1>
+                        <p>Lorem ipsum dolor sit amet, 
+                            consectetur adipiscing elit. 
+                            Vestibulum sollicitudin, metus sit amet vehicula commodo, 
+                            lectus lectus vehicula purus, nec dapibus risus dolor at nisi. 
+                            Suspendisse et iaculis magna, sed malesuada est. 
+                            Nunc vestibulum laoreet tempus. 
+                            Fusce tristique, velit et congue viverra, 
+                            leo libero tristique nisl, nec interdum eros odio eu ante. 
+                            Nunc egestas elementum ipsum, in fermentum diam eleifend id. 
+                            Duis sit amet faucibus tortor, sed auctor ipsum. 
+                            Sed auctor facilisis felis at suscipit. Vestibulum congue dignissim
+                            placerat. Fusce posuere elit in bibendum ultricies. 
+                            Maecenas eget tellus a diam accumsan maximus ut sit amet turpis. 
+                            Orci varius natoque penatibus et magnis dis parturient montes, 
+                            nascetur ridiculus mus. Donec ut ornare odio. Suspendisse et laoreet
+                            mi.</p>
+                        <p>Will be a button with link soon</p>
+                    </ul>
+                </div>
+            </section>
+        </div>
     )
 }
 
