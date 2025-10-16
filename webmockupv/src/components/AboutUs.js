eb85746a17b9e7142ab9926764fcfec647aeac75
filippbrokhman <filipp1303@live.com>
2025-10-16 11:15:35 -0400

    added video functionality. Now, all that's left is to present the mockup and/or convert this into a movable package/container.

--- webmockupv/src/components/AboutUs.js
+++ webmockupv/src/components/AboutUs.js
@@ -73,6 +73,17 @@ const AboutUs = () => {
                     </ul>
                 </div>
             </section>
+            <div className="video-container">
+                <iframe
+                    title="YouTube video about installing windows"
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/qSdD8718reA"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                ></iframe>
+            </div>
         </div> 
     )
 }
